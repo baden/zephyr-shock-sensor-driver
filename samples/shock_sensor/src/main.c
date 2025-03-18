612c94ab2edcef55aa9c09ecc29521ff7e9610ad
Alex <alexprokopov@gmail.com>
2025-03-18 03:43:27 +0200

    змінено приклад

--- samples/shock_sensor/src/main.c
+++ samples/shock_sensor/src/main.c
@@ -31,20 +31,16 @@ static int shock_init()
         return -ENODEV;
     }
  
-    //задаємо максимуми для порогів зон
-    sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_THRESHHOLDS_WARN_MAIN_MAX, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 120, .val2 = 120 });
-    //задаємо на скільки відсотків підвищувати пороги зон за одне загрублення
-    sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_COARSING_WARN_MAIN_PERCENRS, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 20, .val2 = 5 });
     //задаємо мінімальний інтервал між спрацьовуваннями (мс) і максимальний інтервал між спрацьовуваннями (с) при якому скидається загрублення
     sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_TAP_MIN_MAX_INTERVALS, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 5000, .val2 = 30 });
     //задаємо мінімальний інтервал між загрубленнями (c)
     sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_MIN_COARSERING_INTERVAL, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 30, .val2 = 0 });
-    //задаємо початкові пороги зон
-    sensor_attr_set(dev, SENSOR_CHAN_PROX, SENSOR_ATTR_UPPER_THRESH, &(struct sensor_value){ .val1 = 10, .val2 = 60 });
-    //задаємо початкові пороги зон (для загрублення)
-    sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_TRESHHOLDS_INITIAL, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 10, .val2 = 60 });
-    //активуємо сенсор
-    sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_ACTIVE, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = SHOCK_SENSOR_ACTIVE, .val2 = 0 });
+    //задаємо зони попередження
+    sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_WARN_ZONE, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 0, .val2 = 0 });
+    //задаємо основні зони
+    sensor_attr_set(dev, SHOCK_SENSOR_CHANNEL_MAIN_ZONE, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = 0, .val2 = 0 });
+    //задаємо режим
+    sensor_attr_set(dev, SHOCK_SENSOR_MODE, SHOCK_SENSOR_SPECIAL_ATTRS, &(struct sensor_value){ .val1 = SHOCK_SENSOR_MODE_DISARMED, .val2 = 30 });
 
     int rc = sensor_trigger_set(dev, &trig_warn, sensor_warn_handler);
     rc = sensor_trigger_set(dev, &trig_main, sensor_main_handler);
